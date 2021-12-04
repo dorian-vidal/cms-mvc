a8c78e6d277216662570a401a2310c3dbb787407
davy-web <chendavyweb@gmail.com>
2021-12-04 21:20:16 +0100

    Change mise en page

--- app/src/Views/accueilView.php
+++ app/src/Views/accueilView.php
@@ -1,12 +1,20 @@
-<div class="container">
+<!-- titre -->
+<div class="container mt-5">
+    <div class="row">
+        <div class="col text_center_davy">
+            <h1>Accueil</h1>
+            <hr class="hr_davy block_center_davy animation_davy">
+        </div>
+    </div>
+</div>
+
+<div class="container mt-5">
     <div class="row">
 <?php for ($i = 0; $i < count($articles); $i++): ?>
-        <div class="col-lg-3 col-md-4 col-sm-6 text_center_davy">
-            <h1><?= $articles[$i]['titre'] ?></h1><br>
-            <p><?= $articles[$i]['auteur'] ?></p>
-            <p><?= $articles[$i]['date'] ?></p><br>
+        <div class="col-lg-3 col-md-4 col-sm-6 mb-5 text_center_davy">
+            <h2><?= $articles[$i]['titre'] ?></h2><br>
             <img src="<?= URL ?>/src/Views/images/<?= $articles[$i]['image'] ?>" alt="<?= $articles[$i]['titre'] ?>" class="width_full_davy"><br><br>
-            <p><?= $articles[$i]['contenu'] ?></p><br><br>
+            <p><?= $articles[$i]['date'] ?></p><br>
             <!-- bouton_anim_davy -->
             <div class="text_center_davy">
                 <a href="<?= URL ?>/article/<?= $articles[$i]['id_article'] ?>" aria-label="Valider" class="bouton_anim_davy bouton_envoyer" data-text="Voir" title="Voir">
@@ -19,7 +27,7 @@
                     <span>r</span>
                 </a>
             </div><br>
-</div>
+        </div>
 <?php endfor; ?>
     </div>
 </div>
